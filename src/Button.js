0829eaaea9da2551262c99b5daff757925ed2653
clairemuller <clairermuller@gmail.com>
2019-08-25 17:18:30 -0700

    add buttons

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function Button() {
-  return (
-    <div className="button">
-      I'm a button
-    </div>
-  );
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="button">
+        {this.props.label}
+      </div>
+    );
+  }
 }
 
 export default Button;
--- src/Calculator.js
+++ src/Calculator.js
@@ -7,7 +7,30 @@ function Calculator() {
   return (
     <div className="calculator">
       <Screen />
-      <Button />
+      <div className='row' id='row1'>
+        <Button label='0' />
+        <Button label='0' />
+        <Button label='.' />
+        <Button label='=' />
+      </div>
+      <div className='row' id='row2'>
+        <Button label='1' />
+        <Button label='2' />
+        <Button label='3' />
+        <Button label='+' />
+      </div>
+      <div className='row' id='row3'>
+        <Button label='4' />
+        <Button label='5' />
+        <Button label='6' />
+        <Button label='-' />
+      </div>
+      <div className='row' id='row4'>
+        <Button label='7' />
+        <Button label='8' />
+        <Button label='9' />
+        <Button label='x' />
+      </div>
     </div>
   );
 }
