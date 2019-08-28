3afc36c5ef7cf5662d0565d6536b2e7c8e27e1aa
clairemuller <clairermuller@gmail.com>
2019-08-27 17:00:58 -0700

    working on buttons

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ import './App.css';
 class Button extends React.Component {
   render() {
     return (
-      <div className="button">
+      <div className="button" id={this.props.type}>
         <p>{this.props.label}</p>
       </div>
     );
--- src/Calculator.js
+++ src/Calculator.js
@@ -8,28 +8,34 @@ function Calculator() {
     <div className="calculator">
       <Screen />
       <div className='row' id='row1'>
-        <Button label='0' />
-        <Button label='0' />
-        <Button label='.' />
-        <Button label='=' />
+        <Button label='AC' type='action' />
+        <Button label='+/-' type='action' />
+        <Button label='%' type='action' />
+        <Button label='÷' type='action' />
       </div>
       <div className='row' id='row2'>
-        <Button label='1' />
-        <Button label='2' />
-        <Button label='3' />
-        <Button label='+' />
+        <Button label='7' type='input' />
+        <Button label='8' type='input' />
+        <Button label='9' type='input' />
+        <Button label='x' type='action' />
       </div>
       <div className='row' id='row3'>
-        <Button label='4' />
-        <Button label='5' />
-        <Button label='6' />
-        <Button label='-' />
+        <Button label='4' type='input' />
+        <Button label='5' type='input' />
+        <Button label='6' type='input' />
+        <Button label='-' type='action' />
       </div>
       <div className='row' id='row4'>
-        <Button label='7' />
-        <Button label='8' />
-        <Button label='9' />
-        <Button label='x' />
+        <Button label='1' type='input' />
+        <Button label='2' type='input' />
+        <Button label='3' type='input' />
+        <Button label='+' type='action' />
+      </div>
+      <div className='row' id='row5'>
+        <Button label='0' type='input' />
+        <Button label='0' type='input' />
+        <Button label='.' type='input' />
+        <Button label='=' type='action' />
       </div>
     </div>
   );
